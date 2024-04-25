aa2e3e2ecb161e2371d8fb3467cb5c3fe2ba7371
s0urav-hald3r <bishalder96@gmail.com>
2024-04-25 16:22:42 +0530

    Add: Bottom navigation bar controller

--- lib/bindings/general_bindings.dart
+++ lib/bindings/general_bindings.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
+import 'package:mod_game/common/controllers/navigation_bar_controller.dart';
 import 'package:mod_game/feature/home/controllers/page_view_controller.dart';
 import 'package:mod_game/feature/search/controllers/search_controller.dart';
 
 class GeneralBindings extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut(() => NavigationBarController());
     Get.lazyPut(() => PageViewController());
     Get.lazyPut(() => SearchController());
   }

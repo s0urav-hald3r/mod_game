ebbd158a479ffb82048aab09cf9c4e931205664c
s0urav-hald3r <bishalder96@gmail.com>
2024-04-25 16:22:11 +0530

    Add: Bttom navigation bar icons

--- lib/utils/constants/icons.dart
+++ lib/utils/constants/icons.dart
@@ -13,5 +13,18 @@ class XIcon {
   static const throwingIcon = 'assets/icons/throwing_icon.svg';
   static const energyIcon = 'assets/icons/energy_icon.svg';
 
+  // Search Page
   static const searchIcon = 'assets/icons/search_icon.svg';
+
+  // Navigation Bar
+  static const navHomeIcon = 'assets/icons/nav_home_icon.svg';
+  static const navSelectHomeIcon = 'assets/icons/nav_select_home_icon.svg';
+  static const navSearchIcon = 'assets/icons/nav_search_icon.svg';
+  static const navSelectSearchIcon = 'assets/icons/nav_select_search_icon.svg';
+  static const navCommunityIcon = 'assets/icons/nav_community_icon.svg';
+  static const navSelectCommunityIcon =
+      'assets/icons/nav_select_community_icon.svg';
+  static const navDownloadIcon = 'assets/icons/nav_download_icon.svg';
+  static const navSelectDownloadIcon =
+      'assets/icons/nav_select_download_icon.svg';
 }
